e26a8d00d2a22e49259055affafc9aadcda2a998
Qiki <qi@brandingbrand.com>
2014-12-19 00:40:54 -0500

    finished add event page without saving data into the local storage

---
+++ CountDown/Model/EventDetail.m
@@ -0,0 +1,21 @@
+//
+//  EventDetail.m
+//  CountDown
+//
+//  Created by kiki on 12/18/14.
+//  Copyright (c) 2014 kiki. All rights reserved.
+//
+
+#import "EventDetail.h"
+
+@implementation EventDetail
+
+- (void)setEventDescription:(NSString *)eventDescription {
+    _eventDescription = eventDescription;
+}
+
+- (void)setTitle:(NSString *)title {
+    _title = title;
+}
+
+@end
